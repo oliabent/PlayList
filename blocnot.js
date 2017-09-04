0dc59cd8d2f7986d92211a48c5461ec90c9ed847
oliabent <bentkovska.o@gmail.com>
2017-09-04 18:11:30 +0300

    add textarea

---
+++ blocnot.js
@@ -0,0 +1,14 @@
+var elements = document.querySelectorAll('textarea');
+
+function checkValidity() {};
+
+for (i=0; i<elements.length; i++) {
+ (function(element) {
+   var id = element.getAttribute('id');
+   element.value = sessionStorage.getItem(id); // обязательно наличие у элементов id
+   element.oninput = function() {
+     sessionStorage.setItem(id, element.value);
+     checkValidity();
+   };
+ })(elements[i]);
+}
