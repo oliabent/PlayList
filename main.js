1f8e02fd7ac088c596cd2eb4f6053abfdce400e8
oliabent <bentkovska.o@gmail.com>
2017-09-04 12:47:11 +0300

    do some change in files

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ var track6 = document.getElementById('audio6')
 var track7 = document.getElementById('audio7')
 var track8 = document.getElementById('audio8')
 var track9 = document.getElementById('audio9')
+var track10 = document.getElementById('audio10')
+var track11 = document.getElementById('audio11')
+var track12 = document.getElementById('audio12')
+var track13 = document.getElementById('audio13')
+var track14 = document.getElementById('audio14')
+var track15 = document.getElementById('audio15')
+var track16 = document.getElementById('audio16')
+var track17 = document.getElementById('audio17')
+var track18 = document.getElementById('audio18')
+
+
 
 track1.addEventListener("ended",function() {
      
@@ -49,4 +60,51 @@ track8.addEventListener("ended",function() {
      
         track9.play();
     });
+
+track9.addEventListener("ended",function() {
+     
+        track10.play();
+    });
+
+track10.addEventListener("ended",function() {
+     
+        track11.play();
+    });
+
+track11.addEventListener("ended",function() {
+     
+        track12.play();
+    });
+
+track12.addEventListener("ended",function() {
+     
+        track13.play();
+    });
+
+track13.addEventListener("ended",function() {
+     
+        track14.play();
+    });
+
+track14.addEventListener("ended",function() {
+     
+        track15.play();
+    });
+
+track15.addEventListener("ended",function() {
+     
+        track16.play();
+    });
+
+track16.addEventListener("ended",function() {
+     
+        track17.play();
+    });
+
+track17.addEventListener("ended",function() {
+     
+        track18.play();
+    });
+
+
 
